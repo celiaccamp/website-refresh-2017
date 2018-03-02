3490e8d25877be4f5f4f5788d224260332db34a9
Nathaniel Corley <nscorley@gmail.com>
2018-03-01 19:51:41 -0500

    Update firebase.js

--- client/app/firebase.js
+++ client/app/firebase.js
@@ -5,12 +5,12 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: 'AIzaSyDrintM7gHBXQvNb6GcACLgePTcL-82Sj0',
+  apiKey: 'xxxxxxx',
   authDomain: 'camp-celiac.firebaseapp.com',
   databaseURL: 'https://camp-celiac.firebaseio.com',
   projectId: 'camp-celiac',
   storageBucket: 'camp-celiac.appspot.com',
-  messagingSenderId: '91921914286',
+  messagingSenderId: 'xxxxxxxx',
 };
 
 firebase.initializeApp(config);
